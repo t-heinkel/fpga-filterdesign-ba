4b46edb1ececf70c7a07733a4d83ce8154004da5
t-heinkel <timo.heinkel@gmail.com>
2024-10-14 12:28:36 +0200

    added clock wizard to testbench

--- Filterdesign_Bachelorarbeit.srcs/sim_1/new/tb_TOP.vhd
+++ Filterdesign_Bachelorarbeit.srcs/sim_1/new/tb_TOP.vhd
@@ -56,6 +56,18 @@ architecture behavior of tb_TOP is
             audio_out_right_rec     : out STD_LOGIC_VECTOR(BIT_DEPTH-1 downto 0)
         );
     end component;
+    
+    component clk_wiz_0 is
+        Port (
+          -- Clock out ports
+          clk_out1      : out STD_LOGIC;
+          -- Status and control signals
+          reset         : in STD_LOGIC;
+          locked        : out STD_LOGIC;
+          -- Clock in ports
+          clk_in1       : in STD_LOGIC         
+        );
+    end component;
 
     signal clk                  : STD_LOGIC := '0';
     signal clk_del              : STD_LOGIC;
@@ -75,6 +87,11 @@ architecture behavior of tb_TOP is
     
     signal sd_cnt               : STD_LOGIC_VECTOR(2 downto 0) := "000";
     signal ws_cnt               : STD_LOGIC_VECTOR(4 downto 0) := "00000";
+    
+    -- clock wizard signals
+    signal clk_24               : STD_LOGIC;
+    signal clk_locked           : STD_LOGIC;
+    signal clk_reset            : STD_LOGIC;
 
 begin
     -- Instanziierung des zu testenden Systems
@@ -86,13 +103,23 @@ begin
         audio_in_right_trans => tb_audio_right_in,
         audio_out_left_rec => tb_audio_left_out,
         audio_out_right_rec => tb_audio_right_out,
-        sck => clk_25,
+        sck => clk_24,
         ws => ws,
         sd_in => sd_in,
         sd_out => sd_out,
         rx_ready => tb_rx_ready,
         tx_ready => tb_tx_ready
     );
+    
+     clk_wizard : clk_wiz_0
+        port map (
+          clk_out1 => clk_24,
+          -- Status and control signals
+          reset => clk_reset,
+          locked => clk_locked,
+          -- Clock in ports
+          clk_in1 => clk
+        );       
 
     tb_process : process
     begin
@@ -131,7 +158,7 @@ begin
     
     tb_rx_ready <= '1';
     
-    sd_in_process : process(clk, clk_25)
+    sd_in_process : process(clk, clk_24)
     begin
         if rising_edge(clk) then
             tb_rx_ready <= '0';
@@ -139,7 +166,7 @@ begin
             --for testing
             --sd_cnt <= std_logic_vector(unsigned(sd_cnt) + 1);
             
-            if rising_edge(clk_25) then
+            if rising_edge(clk_24) then
                 
                 if(sd_cnt = "000") then
                     sd_value <= not sd_value;
