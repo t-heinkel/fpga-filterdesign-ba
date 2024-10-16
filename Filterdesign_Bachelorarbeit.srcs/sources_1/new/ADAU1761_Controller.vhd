2ae8f70a4be7ece857081de32a5c2fecd339108a
t-heinkel <timo.heinkel@gmail.com>
2024-10-16 15:28:41 +0200

    started with adau-controller and i2c-master

---
+++ Filterdesign_Bachelorarbeit.srcs/sources_1/new/ADAU1761_Controller.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.10.2024 12:43:00
+-- Design Name: 
+-- Module Name: ADAU1761_Controller - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ADAU1761_Controller is
+--  Port ( );
+end ADAU1761_Controller;
+
+architecture Behavioral of ADAU1761_Controller is
+
+begin
+
+
+end Behavioral;
---
+++ Filterdesign_Bachelorarbeit.srcs/sources_1/new/I2C_Master.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.10.2024 12:43:00
+-- Design Name: 
+-- Module Name: I2C_Master - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity I2C_Master is
+--  Port ( );
+end I2C_Master;
+
+architecture Behavioral of I2C_Master is
+
+begin
+
+
+end Behavioral;
